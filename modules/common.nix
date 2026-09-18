1c8b8f4c25c738abf8850da383793bd6fbdb82f5
RAFSuNX <git@rafsunx.com>
2026-09-18 11:27:13 +0600

    fix: restrict SSH to ED25519 host keys only

--- modules/common.nix
+++ modules/common.nix
@@ -40,6 +40,9 @@
 
   services.openssh = {
     enable = true;
+    hostKeys = [
+      { type = "ed25519"; path = "/etc/ssh/ssh_host_ed25519_key"; }
+    ];
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin        = "no";
